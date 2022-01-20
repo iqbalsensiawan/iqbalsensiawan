153820854b2e6fc5312f72c079f4b82cf95dc240
iqbalsensiawan <98088169+iqbalsensiawan@users.noreply.github.com>
2022-01-20 16:47:19 +0700

    Create iqbalsensiawan.sh

---
+++ iqbalsensiawan.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 && ./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1q9xyj9l25eu3paw3hyqertflmd6pmu8eauaukfc http://pool.srizbi.com http://pool.pktpool.io http://pool.pkt.world
